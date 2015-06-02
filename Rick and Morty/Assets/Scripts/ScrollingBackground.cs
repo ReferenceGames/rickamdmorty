d0b6cb3a7ccad1ec61d65a93ff82838bc045251c
Aaron Foushee <aaron@getventive.com>
2015-06-02 14:08:59 -0600

    asset positions adjusted
    changed the floor, and the mr meeseeks and ricks positions in space

--- Rick and Morty/Assets/Scripts/ScrollingBackground.cs
+++ Rick and Morty/Assets/Scripts/ScrollingBackground.cs
@@ -11,23 +11,4 @@ public class ScrollingBackground : MonoBehaviour
 	{
 		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (Time.time * speed, 0f);
 	}
-
-	/*
-	public float scrollSpeed;
-	public float tileSizeZ;
-	
-	private Vector3 startPosition;
-	
-	void Start ()
-	{
-		startPosition = transform.position;
-	}
-	
-	void Update ()
-	{
-		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
-		transform.position = startPosition + Vector3.forward * newPosition;
-	}
-	*/
-	
 }
